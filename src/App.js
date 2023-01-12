a4e8dd423cf4e47e3e1aaeb7d820344000f66354
Victor Buga <bugavictor86@gmail.com>
2023-01-12 14:32:50 +0100

    remove basename

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import "./App.css";
 function App() {
   return (
     // <Router basename="/cv2/">
-    <Router basename="/cv2/" >
+    <Router >
       <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
